f1480e080f600f5f87f076ea171b638a40a5c138
Ben Mann <paperandcolours@gmail.com>
2016-05-12 13:51:44 +0100

    updated notes

--- notes_v2.js
+++ notes_v2.js
@@ -58,39 +58,177 @@ function assert(condition, message) {
 		throw message || "Assertion failed";
 	}
 }
-function getModifier(mod_string) {
-	var modifier = 0;
+function accidentalsToInt(mod_string) {
+	var accidentals = 0;
 	for (var i=0; i<mod_string.length; i++) {
-		if (mod_string[i] == "b") modifier--;
-		if (mod_string[i] == "#") modifier++;
+		if (mod_string[i] == "b") accidentals--;
+		if (mod_string[i] == "#") accidentals++;
 	}
-	return modifier;
+	return accidentals;
 };
+
+function intToAccidental(mod_int) {
+	if (mod_int >6) mod_int -= 12;
+	if (mod_int < -6) mod_int += 12;
+	var accidentals = "";
+	if (mod_int > 0) {
+		for (var i=0; i<mod_int; i++) {
+			accidentals += "#";
+		}
+	}
+	else if (mod_int<0) {
+		for (var i=0; i>mod_int; i--) {
+			accidentals+= "b";
+		}
+	}
+	return accidentals;
+};
+
+function noteToSemitones(note) {
+		return {
+		C:0,
+		D:2,
+		E:4,
+		F:5,
+		G:7,
+		A:9,
+		B:11
+		}[note];
+}
+
+
+function semitonesToNote(note) {
+		return {
+		0: "C",
+		2: "D",
+		4: "E",
+		5: "F",
+		7: "G",
+		9: "A",
+		11: "B",
+		}[note];
+}
+
+function degreeToSemitones(note) {
+		return {
+		1:0,
+		2:2,
+		3:4,
+		4:5,
+		5:7,
+		6:9,
+		7:11
+		}[note];
+}
+
+function noteToInt(note) {
+		return {
+		C:1,
+		D:2,
+		E:3,
+		F:4,
+		G:5,
+		A:6,
+		B:7
+		}[note];
+}
+function intToNote(i) {
+		while (i>7) i-= 7;
+		while (i<1) i+= 7;
+		return {
+		1:'C',
+		2:'D',
+		3:'E',
+		4:'F',
+		5:'G',
+		6:'A',
+		7:'B'
+		}[i];
+}
+
 
 function Interval(name) {
-	this.name = name;
 	var re_validInterval = /^[b#]*\d+$/;
+	var re_degree = name.search(/\d+$/);
 	assert(re_validInterval.test(name), "Invalid interval name");
 
-	var degrees_semitones = { 
-		1: 0,
-		2: 2,
-		3: 4,
-		4: 5,
-		5: 7,
-		6: 9,
-		7: 11
-	};
-	var degree_start = this.name.search(/\d+$/);
-	var modifiers = this.name.slice(0, degree_start);
-	this.degree = parseInt(this.name.slice(degree_start));
-	var base_semitones = degrees_semitones[this.degree];
+	this.accidentals = accidentalsToInt(name.slice(0, re_degree))
+	this.degree = parseInt(name.slice(re_degree));
 
-	var modifier = getModifier(modifiers);
-	this.semitones = base_semitones + modifier;
+	this.octave = 0;
+	while (this.degree > 7) {
+		this.octave++;
+		this.degree -= 7;
+	}
+	this.semitones = degreeToSemitones(this.degree) + this.accidentals + this.octave*12;
+
+	this.interval = function() {
+		return intToAccidental(this.accidentals) + String(this.degree + this.octave*7);
+	}
 };
 
 
+function Note(note) {
+	if (typeof(note) == "string") {
+		var re_validNote = /[A-G][b#]*[-]?\d/;
+		var re_octave = note.search(/[-]?\d+$/);
+		assert(re_validNote.test(note), "Invalid note name");
+		this.note_name = note[0];
+		this.accidentals = accidentalsToInt(note.slice(1, re_octave));
+		this.octave = parseInt(note.slice(re_octave));
+
+		this.semitones = noteToSemitones(this.note_name) + this.accidentals + this.octave*12;
+
+	}
+	this.note = function() {
+		return String(this.note_name) + intToAccidental(this.accidentals) + String(this.octave);
+	}
+
+	this._genNote = function(target_note, sum_semitones) {
+		var target_octave = 0;
+		while (sum_semitones >= 12) {
+			target_octave++;
+			sum_semitones-= 12;
+		}
+		while (sum_semitones < 0) {
+			target_octave--;
+			sum_semitones+= 12;
+		}
+		var degree_semitones = noteToSemitones(target_note);
+		var accidentals = intToAccidental(sum_semitones - degree_semitones);
+		return new Note(target_note+accidentals+target_octave);
+	}
+
+	this.add = function(interval) {
+		assert(interval instanceof Interval, "Can only add Intervals to Notes");
+		var target_note = intToNote(noteToInt(this.note_name) + interval.degree -1);
+		var sum_semitones = this.semitones + interval.semitones;
+		return this._genNote(target_note, sum_semitones);
+
+	};
+	this.sub = function(value) {
+		if (value instanceof Interval) {
+			var interval = value;
+			var target_note = intToNote(noteToInt(this.note_name) - interval.degree +1);
+			var sum_semitones = this.semitones - interval.semitones;
+			return this._genNote(target_note, sum_semitones);
+		}
+		else if (value instanceof Note) {
+			var target = value;
+			var difference_semitones = target.semitones - this.semitones;
+			var difference_degrees = noteToInt(target.note_name) - noteToInt(this.note_name) +1;
+
+			var degree_reference = degreeToSemitones(difference_degrees);
+			var modifier = intToAccidental(difference_semitones - degree_reference);
+			console.log(modifier)
+			console.log(degree_reference)
+			console.log(difference_semitones)
+			console.log(difference_degrees)
+			return new Interval(modifier+difference_degrees);
+		}
+		else throw "Can only subtract Notes or Intervals from Notes";
+	};
+}
 
 function ScaleFormula(interval_list) {
 	assert(typeof(interval_list) == "string", "ScaleFormula must take a string for now")
@@ -99,45 +237,6 @@ function ScaleFormula(interval_list) {
 	for (var i=0; i< interval_list.length; i++) {
 		this.intervals.push(new Interval(interval_list[i]));
 	}
-}
-
-function Note(note) {
-	if (typeof(note) == "string") {
-		var re_validNote = /[A-G][b#]*\d/;
-		assert(re_validNote.test(note), "Invalid note name");
-		this.note = note;
-		var notes_int = {
-		A:0,
-		B:2,
-		C:3,
-		D:5,
-		E:7,
-		F:8,
-		G:10
-		}
-		this.idx = (notes_int[note[0]] + getModifier(note.slice(1)))%12;
-	}
-	else if (typeof(note) == "number") {
-		this.note_int = note % 12;
-		var int_notes = {
-		0: "A",
-		1: "A#",
-		2: "B",
-		3: "C",
-		4: "C#",
-		5: "D",
-		6: "D#",
-		7: "E",
-		8: "F",
-		9: "F#",
-		10: "G",
-		11: "G#",
-		};
-		this.note = int_notes[this.note_int];
-	}
-	this.add = function(interval) {
-		return new Note((this.note_int + interval.semitones)%12);
-	};
 }
 
 function Scale(root, formula) {
